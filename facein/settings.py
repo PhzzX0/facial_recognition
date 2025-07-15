feaa542367a1dc892afbbde066227854617c274d
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-07-15 15:12:47 -0300

    atualização para funcionar com o render

--- facein/settings.py
+++ facein/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pw^t$yzd2-+gu#v*q4y6n=nzu7*hti_=c4-+z2*2c+zri6ilye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
 
 
 # Application definition
