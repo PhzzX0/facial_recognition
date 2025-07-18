3b3435c1741d767f1d3555abf4f5062e75707c83
Pedro Henrique de Oliveira Freitas <20221084010003@ifrn.local>
2025-07-18 16:22:15 -0300

    cadastrando alguns rostos pra testes

--- facein/urls.py
+++ facein/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
+    path('video_feed/', views.video_feed, name='video_feed'),
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('login/', views.login, name="login"),
